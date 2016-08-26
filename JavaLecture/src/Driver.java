db26377a4437dd7f33dde7f980976bf8fca547be
Aaresh Bachana <Aaresh Bachana>
2016-08-26 15:36:42 -0600

    Different Class Files added

---
+++ JavaLecture/src/Driver.java
@@ -0,0 +1,37 @@
+
+public class Driver {
+
+	public static void main(String[] args) 
+	{
+		//Person aaresh = new Person ("Aaresh Bachana", 5);
+		//System.out.println(aaresh.getName());
+		//System.out.println(aaresh);
+		
+		Faculty liz = new Faculty();
+		//liz.printPhonenumber();
+		
+		Student CookieMonster = new Student("Cookie Monster", 5, 3.0);
+		System.out.println(CookieMonster.getName());
+		System.out.println(CookieMonster);
+		
+		Person[] people = new Person[3];
+		people[0] = liz;
+		people[1] = CookieMonster;
+		people[2] = new Student("Sassy", 21, 3.97);
+		
+		for(int i = 0; i < people.length; i++)
+		{
+			System.out.println(people[i].getName());
+			
+			if(people[i] instanceof Student) //can be used to check whether it is an instance of a certain class
+			{
+				Student s = (Student) people[i];
+				System.out.println(s.getGpa());
+			}
+		}
+		
+		Grader[] graders = new Grader[2];
+		graders[0] = liz;
+	}
+
+}
